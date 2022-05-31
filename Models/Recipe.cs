62317ef7eb455af5b90cacd5826f908f7b3009fc
yokoso <tomsolga90@gmail.com>
2022-05-31 14:12:09 +0200

    add model validation

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -8,16 +8,25 @@ namespace KukBuk.Models
     {
         [Key]
         public int RecipeId { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         [Display (Name = "Recipe Name")]
+        [StringLength(60, MinimumLength =3)]
+        [Required]
         public string? Title { get; set; }
+
+        [Range(0, 5000)]
         public string? Description { get; set; }
         [Display (Name = "Photo")]
+
+        [RegularExpression(@"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)")]
         public string? PhotoUrl { get; set; }
 
         [Display (Name = "Upload Date")]
         [DataType(DataType.Date)]
         public DateTime UploadDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         [Display (Name = "Cuisine")]
         public string? Cuisine { get; set; }
     }
